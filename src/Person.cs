ec15096225bcbbd7cf4ba8ed7c71ff329d4abb12
Erik Heumrønningen <erik.heumronningen@creuna.no>
2020-05-05 20:49:44 +0200

    Add tests for going into isolation.

--- src/Person.cs
+++ src/Person.cs
@@ -4,15 +4,25 @@ namespace GameOfCorona
 {
     public class Person
     {
+        bool _hasMetInfectedPerson;
         public bool IsInfected { get; set; }
+        public bool IsInIsolation { get; set; }
 
         public void Meet(Person person, double probability)
         {
             if(CheckRandom(probability))
                 IsInfected = person.IsInfected;
+
+            _hasMetInfectedPerson = person.IsInfected;
         }
 
-        bool CheckRandom(in double probability)
+        public void Sleep(double pIsolation)
+        {
+            if(CheckRandom(pIsolation))
+                IsInIsolation = _hasMetInfectedPerson;
+        }
+
+        static bool CheckRandom(in double probability)
         {
             var random = new Random();
             var randomFactor = random.Next(0, 100) / 100.0;
--- tests/GameOfCorona.UnitTests/GameTests.cs
+++ tests/GameOfCorona.UnitTests/GameTests.cs
@@ -16,5 +16,21 @@ namespace GameOfCorona.UnitTests
             
             Assert.Equal(expected, person.IsInfected);
         }
+        
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(0, false)]
+        public void When_a_healthy_person_is_in_contact_with_an_infected_person_Should_go_into_isolation(double probability, bool expected)
+        {
+            var person = new Person();
+            var infectedPerson = new Person {IsInfected = true};
+            
+            person.Meet(infectedPerson, 0);
+            person.Sleep(probability);
+            
+            Assert.Equal(expected, person.IsInIsolation);
+        }
+        
+        //An infected person goes into quarantine 
     }
 }
